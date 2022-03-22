f7c3299df1886f968f130912a8da61fa3c3367f0
Роман <44298519+RRasputin92@users.noreply.github.com>
2022-03-22 16:02:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,7 @@ from longpoll_bot import LongPollBot
 from nlu_longpoll_bot import NLULongPollBot
 
 if __name__ == '__main__':
-    # создание и запуск обычного бота
-    bot = Bot()
-
-    # отправка тестового сообщения
-    bot.send_message()
-
-    # отправка сообщения с заданными параметрами
-    bot.send_message(receiver_user_id="1234567890", message_text="Привет, это сообщение отправлено автоматически")
-
+    nlu_longpoll_bot = NLULongPollBot()
+    nlu_longpoll_bot.run_long_poll()
 
 
