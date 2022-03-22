f47c80db71ff45e90ea9ec8cfa1a1f616d3d3064
Роман <44298519+RRasputin92@users.noreply.github.com>
2022-03-22 22:28:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from longpoll_bot import LongPollBot
 from nlu_longpoll_bot import NLULongPollBot
 
 if __name__ == '__main__':
-    long_poll_bot = LongPollBot()
-    long_poll_bot.run_long_poll()
+    nlu_longpoll_bot = NLULongPollBot()
+    nlu_longpoll_bot.run_long_poll()
 
+    
